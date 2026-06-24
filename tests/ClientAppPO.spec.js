9bcaaf545cf88c3a0d6efe159ba47a412d25c106
saifulsahim <sahimalam96@gmail.com>
2026-06-24 20:18:40 +0600

    Parameterization in running test with different data sets

--- tests/ClientAppPO.spec.js
+++ tests/ClientAppPO.spec.js
@@ -3,30 +3,31 @@ const { POManager } = require('../pageobjects/POManager');
 // JSON -> string -> JS object
 const dataset = JSON.parse(JSON.stringify(require("../utils/placeOrderTestData.json")));
 
-test('End to end ecommerce flow', async ({ page }) => {
-    const poManager = new POManager(page);
-    const loginPage = poManager.getLoginPage();
-    const dashboardPage = poManager.getDashboardPage();
-    const cartPage = poManager.getCartPage();
-    const checkoutPage = poManager.getCheckOutPage();
-    const orderHistoryPage = poManager.getOrderHistoryPage();
+for (const data of dataset) {
+    test(`End to end ecommerce flow ${data.productName}`, async ({ page }) => {
+        const poManager = new POManager(page);
+        const loginPage = poManager.getLoginPage();
+        const dashboardPage = poManager.getDashboardPage();
+        const cartPage = poManager.getCartPage();
+        const checkoutPage = poManager.getCheckOutPage();
+        const orderHistoryPage = poManager.getOrderHistoryPage();
 
-    await loginPage.goTo();
-    await loginPage.validLogin(dataset.username, dataset.password);
+        await loginPage.goTo();
+        await loginPage.validLogin(data.username, data.password);
 
-    await dashboardPage.searchProductAddToCart(dataset.productName);
-    await dashboardPage.navigateToCart();
+        await dashboardPage.searchProductAddToCart(data.productName);
+        await dashboardPage.navigateToCart();
 
-    await cartPage.verifyProductIsDisplayed(dataset.productName);
-    await cartPage.Checkout();
+        await cartPage.verifyProductIsDisplayed(data.productName);
+        await cartPage.Checkout();
 
-    await checkoutPage.searchCountryAndSelect("ban", "Bangladesh");
-    await checkoutPage.VerifyEmailId(dataset.username);
-    const orderId = await checkoutPage.submitAndGetOrderId();
-    await dashboardPage.navigatoToOrders();
+        await checkoutPage.searchCountryAndSelect("ban", "Bangladesh");
+        await checkoutPage.VerifyEmailId(data.username);
+        const orderId = await checkoutPage.submitAndGetOrderId();
+        await dashboardPage.navigatoToOrders();
 
-    await orderHistoryPage.searchOrderAndSelect(orderId);
-    expect(orderId.includes(await orderHistoryPage.getOrderId())).toBeTruthy();
+        await orderHistoryPage.searchOrderAndSelect(orderId);
+        expect(orderId.includes(await orderHistoryPage.getOrderId())).toBeTruthy();
 
-});
-
+    });
+}
